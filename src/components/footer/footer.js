541e7d111cf45e60c5e989978069e0058a09b9d1
hedlundemma <emmahedlund93@gmail.com>
2023-09-14 16:07:29 +0200

    fixed footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,13 +1,13 @@
-"use client"
+"use client";
 import Link from "next/link";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const FooterSection= styled.footer`
+const FooterSection = styled.footer`
   background-color: #000000;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  color:white;
+  color: white;
   width: 100%;
   gap: 32px;
   padding: 24px;
@@ -22,58 +22,49 @@ const LogoDiv = styled.div`
 `;
 
 const Logo = styled.img`
-    height: 36px;
-    width: 36px;
+  height: 36px;
+  width: 36px;
 `;
-const Name = styled.h2`
-
-`;
+const Name = styled.h2``;
 
 const FooterDiv = styled.div`
-
- display: flex;
- flex-direction:row;
- div{
   display: flex;
-  flex-direction: column;
-  gap: 10px;
- }
+  flex-direction: row;
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
 
- justify-content: space-between;
-
- 
+  justify-content: space-between;
 `;
 
-
-
-
-
-const LowerDiv= styled.div`
+const LowerDiv = styled.div`
   p {
     margin: 0;
   }
 `;
 
-export default function Footer () {
-    return (
-        <FooterSection>
-          <LogoDiv>
-            <Logo src="logo.svg"></Logo>
-            <Name>Submind</Name>
-          </LogoDiv>
-          <FooterDiv>
-            <div>
-              <p>Kontakta oss</p>
-              <p>FAQ</p>
-            </div>
-            <div>
-              <Link href = "./userterms">Användarvillkor</Link>
-              <p>Integritetspolicy</p>
-            </div>
-          </FooterDiv>
-          <LowerDiv>
-            <p>© Submind 2023.</p>
-          </LowerDiv>
-        </FooterSection>
-    )
+export default function Footer() {
+  return (
+    <FooterSection>
+      <LogoDiv>
+        <Logo src="logo.svg"></Logo>
+        <Name>Submind</Name>
+      </LogoDiv>
+      <FooterDiv>
+        <div>
+          <p>Kontakta oss</p>
+          <p>FAQ</p>
+        </div>
+        <div>
+          <Link href="./userterms">Användarvillkor</Link>
+          <p>Integritetspolicy</p>
+        </div>
+      </FooterDiv>
+      <LowerDiv>
+        <p>© Submind 2023.</p>
+      </LowerDiv>
+    </FooterSection>
+  );
 }
